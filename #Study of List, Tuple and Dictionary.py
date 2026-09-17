0a324c865c174ca70103f0603a90a68cfeb31784
riteshdamahe11 <riteshdamahe96@gmail.com>
2026-09-17 22:28:42 +0530

    Create #Study of List, Tuple and Dictionary.py

---
+++ #Study of List, Tuple and Dictionary.py
@@ -0,0 +1,18 @@
+#Study of List, Tuple and Dictionary 
+#1st List is ordered,indexed,mutable and allows duplicate elements,denoted by [] and traversed by for loop
+list=[1,20,"M","omkar"]
+print(list)
+#2nd Tuple is ordered,indexed,immutable and allows duplicate elements,denoted by () and traversed by for loop
+tuple=(1,20,"M","omkar","Nagpur")
+print(tuple)
+#3 dictionary is unordered collection of key value pairs,keys are unique and values can be duplicate,dict is mutable dinoted by {} and key value pairs are separated by colon(:)
+dict={
+    "Name":"Omkar",
+    "Age":20,
+    "City":"Nagpur"
+
+}
+print(dict)
+#set is unoredered collection of unique elements,nonindexable,only unique elements are allowed in set and muttable
+set={1,2,3,4,5}
+print(set)
